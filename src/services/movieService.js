0e2026bcfbca24051f60a6a2ba9a9951a1885e6b
Mariyan Yordanov <marsrewq@gmail.com>
2025-05-31 20:00:31 +0300

    feat: update search form to use 'title' instead of 'search' for improved filtering

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -40,8 +40,8 @@ const movies = [
 export default {
     getAllMovies(filter = {}) {
         let result = movies.slice();
-        if (filter.search) {
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        if (filter.title) {
+            result = result.filter(movie => movie.title.toLowerCase().includes(filter.title.toLowerCase()));
         }
         if (filter.genre) {
             result = result.filter(movie => movie.genre.localeCompare(filter.genre, undefined, { sensitivity: 'base' }) === 0);
