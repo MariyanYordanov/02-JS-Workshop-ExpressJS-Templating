50117be3b3bf4ce81912ef9ec12457b99bfd331b
Mariyan Yordanov <marsrewq@gmail.com>
2025-05-31 11:21:26 +0300

    feat: add express-handlebars for templating and set up initial views
    - Updated package.json and package-lock.json to include express-handlebars.
    - Configured handlebars as the view engine in index.js.
    - Created main layout and home view templates.
    - Added empty partials for footer and navigation.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
+// Init express instance
 const app = express();
+
+// Add and coning view engine
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'main', 
+    layoutsDir: 'src/views/layouts',
+    partialsDir: 'src/views/partials',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.render('home');
 });
 
 app.listen(3000, () => {
