675430b424f23113cc2be08595e6b4951d34e890
Mariyan Yordanov <marsrewq@gmail.com>
2025-05-31 11:38:59 +0300

    fix: update image paths and improve static file serving

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import handlebars from "express-handlebars";
 // Init express instance
 const app = express();
 
+// Middleware to serve static files
+app.use(express.static('src/public'));
+
 // Add and coning view engine
 app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-    defaultLayout: 'main', 
-    layoutsDir: 'src/views/layouts',
-    partialsDir: 'src/views/partials',
+    extname: 'hbs'
 }));
 
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
 app.get("/", (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 app.listen(3000, () => {
-  console.log("Server is listening on http://localhost:3000");
+    console.log("Server is listening on http://localhost:3000");
 });
